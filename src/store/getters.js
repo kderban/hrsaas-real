6f7e6019391f5a86803c46f3420f8ff5e81e282a
liuyunfeng <kderban123@163.com>
2022-03-19 13:33:01 +0800

    封装获取用户资料的action并共享用户状态

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,6 +1,7 @@
 const getters = {
   sidebar: state => state.app.sidebar,
   device: state => state.app.device,
-  token: state => state.user.token // 建立token的快捷访问。 在根级的getters上 开发子模块的属性给别人看 给别人用
+  token: state => state.user.token, // 建立token的快捷访问。 在根级的getters上 开发子模块的属性给别人看 给别人用
+  name: state => state.user.userInfo.username // 建立对于用户名的快捷访问
 }
 export default getters
--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,8 +1,9 @@
 import { getToken, setToken, removeToken } from '@/utils/auth'
-import { login } from '@/api/user'
+import { login, getUserInfo } from '@/api/user'
 // 状态
 const state = {
-  token: getToken() // 设置token为共享状态。初始化vuex的时候，就先从缓存中读取
+  token: getToken(), // 设置token为共享状态。初始化vuex的时候，就先从缓存中读取
+  userInfo: {} // 定义一个空的对象 不是null 因为后边要开发userInfo的属性给别人用  userInfo.name
 }
 // 修改状态
 const mutations = {
@@ -13,6 +14,13 @@ const mutations = {
   removeToken (state) {
     state.token = null // 删除vuex的token
     removeToken() // 先清除 vuex  再清除缓存 vuex和 缓存数据的同步
+  },
+  setUserInfo (state, result) {
+    // 更新一个对象
+    state.userInfo = result
+  },
+  removeUserInfo (state) {
+    state.userInfo = {}
   }
 }
 // 执行异步
@@ -20,6 +28,11 @@ const actions = {
   async login (context, data) {
     const result = await login(data) // 拿到token
     context.commit('setToken', result) // 设置token
+  },
+  async getUserInfo (context) {
+    const result = await getUserInfo() // 获取返回值
+    context.commit('setUserInfo', result) // 提交到mutations
+    return result // 这里给后期做权限的时候，留下的伏笔
   }
 }
 
