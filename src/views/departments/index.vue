cec430dde14a3c909d2281182f10368f5314b7b9
liuyunfeng <kderban123@163.com>
2022-03-22 11:28:20 +0800

    获取组织架构数据，并进行树形处理

--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -3,13 +3,16 @@
     <div class="app-container">
       <!-- 组织架构内容-头部 -->
       <el-card class="tree-card">
-         <!-- 缺少treeNode -->
-         <!-- :is-root="true" 是根结点 -->
-        <tree-tools :tree-node="company" :is-root="true" />
+        <!-- 缺少treeNode -->
+        <!-- :is-root="true" 是根结点 -->
+        <tree-tools
+          :tree-node="company"
+          :is-root="true"
+        />
 
         <!-- 放置一个属性   这里的props和我们之前学习的父传子 的props没关系 -->
-          <!-- 传入内容 插槽内容 会循环多次 有多少节点 就循环多少次 -->
-          <!-- 作用域插槽 slot-scope="obj" 接收传递给插槽的数据   data 每个节点的数据对象-->
+        <!-- 传入内容 插槽内容 会循环多次 有多少节点 就循环多少次 -->
+        <!-- 作用域插槽 slot-scope="obj" 接收传递给插槽的数据   data 每个节点的数据对象-->
         <el-tree
           :data="departs"
           :props="defaultProps"
@@ -31,6 +34,10 @@
 
 <script>
 import TreeTools from './components/tree-tools.vue'
+
+import { getDepartments } from '@/api/departments'
+
+import { tranListToTreeData } from '@/utils'
 export default {
   components: {
     TreeTools
@@ -44,6 +51,18 @@ export default {
       departs: [{ name: '总裁办', manager: '曹操', children: [{ name: '董事会', manager: '曹丕' }] },
         { name: '行政部', manager: '刘备' },
         { name: '人事部', manager: '孙权' }]
+    }
+  },
+  created () {
+    this.getDepartments() // 调用自身的方法
+  },
+  methods: {
+    async getDepartments () {
+      const result = await getDepartments()
+      this.company = { name: result.companyName, manager: '负责人' }
+      // 这里定义一个空串  因为 它是根 所有的子节点的数据pid 都是 ""
+      this.departs = tranListToTreeData(result.depts, '')
+      console.log(result)
     }
   }
 }
