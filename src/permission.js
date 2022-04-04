3d8a1ff3a47b1fdd59573b6841de7e35928658e4
liuyunfeng <kderban123@163.com>
2022-04-04 16:20:29 +0800

    前端权限应用-页面访问和菜单

--- src/permission.js
+++ src/permission.js
@@ -25,9 +25,20 @@ router.beforeEach(async (to, from, next) => {
       // 如果当前vuex中有用户的资料的id，表示已经有资料了不需要获取，没有id的话才需要获取
       if (!store.getters.userId) {
         // 如果后续需要根据用户资料获取数据的话，这里必须改成同步
-        await store.dispatch('user/getUserInfo')
+        // async 函数所return的内容，await可以接收到
+        // actions中函数 默认是Promise对象 调用这个对象 想要获取返回的值话 必须 加 await或者是then
+        // actions是做异步操作的
+        const { roles } = await store.dispatch('user/getUserInfo')
+        const routes = await store.dispatch('permission/filterRoutes', roles.menus) // 筛选得到当前用户可用的动态路由
+        // 动态路由 添加到 路由表中 默认的路由表 只有静态路由 没有动态路由
+        // addRoutes  必须 用 next(地址) 不能用next()
+        router.addRoutes(routes) // 添加动态路由到路由表  铺路
+        // 添加完动态路由之后
+        next(to.path) // 相当于跳到对应的地址  相当于多做一次跳转 为什么要多做一次跳转
+        // 进门了，但是进门之后我要去的地方的路还没有铺好，直接走，掉坑里，多做一次跳转，再从门外往里进一次，跳转之前 把路铺好，再次进来的时候，路就铺好了
+      } else {
+        next()
       }
-      next()
     }
   } else {
     // 没有token的情况下
--- src/store/getters.js
+++ src/store/getters.js
@@ -5,6 +5,7 @@ const getters = {
   name: state => state.user.userInfo.username, // 建立对于用户名的快捷访问
   userId: state => state.user.userInfo.userId,
   staffPhoto: state => state.user.userInfo.staffPhoto, // 建立头像的快捷访问
-  companyId: state => state.user.userInfo.companyId // 建立对于user模块的companyId的快捷访问
+  companyId: state => state.user.userInfo.companyId, // 建立对于user模块的companyId的快捷访问
+  routes: state => state.permission.routes // 导出当前的路由
 }
 export default getters
