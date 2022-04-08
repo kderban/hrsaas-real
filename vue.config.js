a79712f0bffcb8c1faf5ca889d1a9bf32737812d
liuyunfeng <kderban123@163.com>
2022-04-08 17:11:14 +0800

    注入CDN文件到模板

--- vue.config.js
+++ vue.config.js
@@ -15,6 +15,31 @@ const name = defaultSettings.title || 'vue Admin Template' // page title
 // port = 9528 npm run dev OR npm run dev --port = 9528
 const port = process.env.port || process.env.npm_config_port || 9528 // dev port
 
+let cdn = { css: [], js: [] }
+let externals = {}
+// 通过环境变量 来区分是否使用cdn
+const isProd = process.env.NODE_ENV === 'production' // 判断是否是生产环境
+if (isProd) {
+  // 如果是生产环境 就排除打包 否则不排除
+  externals = {
+    // key(包名) / value(这个值 是 需要在CDN中获取js, 相当于 获取的js中 的该包的全局的对象的名字)
+    'vue': 'Vue', // 后面的名字不能随便起 应该是 js中的全局对象名
+    'element-ui': 'ELEMENT', // 都是js中全局定义的
+    'xlsx': 'XLSX' // 都是js中全局定义的
+  }
+  cdn = {
+    css: [
+      'https://unpkg.com/element-ui/lib/theme-chalk/index.css' // 提前引入elementUI样式
+    ], // 放置css文件目录
+    js: [
+      'https://unpkg.com/vue/dist/vue.js', // vuejs
+      'https://unpkg.com/element-ui/lib/index.js', // element
+      'https://cdn.jsdelivr.net/npm/xlsx@0.16.6/dist/xlsx.full.min.js', // xlsx 相关
+      'https://cdn.jsdelivr.net/npm/xlsx@0.16.6/dist/jszip.min.js' // xlsx 相关
+    ] // 放置js文件目录
+  }
+}
+
 // All configuration item explanations can be find in https://cli.vuejs.org/config/
 module.exports = {
   /**
@@ -46,6 +71,7 @@ module.exports = {
     }
     // before: require('./mock/mock-server.js')
   },
+  // 配置webpack的属性
   configureWebpack: {
     // provide the app's title in webpack's name field, so that
     // it can be accessed in index.html to inject the correct title.
@@ -54,8 +80,12 @@ module.exports = {
       alias: {
         '@': resolve('src')
       }
-    }
+    },
+    // 排除 elementUI xlsx  和 vue
+    // key(要排除的包名):value(实际引入的包的全局变量名)
+    externals: externals
   },
+  // webpack执行链
   chainWebpack (config) {
     // it can improve the speed of the first screen, it is recommended to turn on preload
     config.plugin('preload').tap(() => [
@@ -67,6 +97,14 @@ module.exports = {
         include: 'initial'
       }
     ])
+
+    // 注入cdn变量
+    // 这行代码会在打包的时候执行
+    config.plugin('html').tap(args => {
+      // args是我们注入html模版的一个变量
+      args[0].cdn = cdn // 后面的cdn就是上面 定义的变量
+      return args // 需要返回这个参数
+    })
 
     // when there are many pages, it will cause too many meaningless requests
     config.plugins.delete('prefetch')
